42d4e5759e9c115c63c02f1d96c7944fe58c88a8
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-05-20 23:52:02 -0300

    Ajusta mensagem flash

--- src/nutri_app/utils/decorators.py
+++ src/nutri_app/utils/decorators.py
@@ -15,7 +15,7 @@ def perfil_completo_required(f):
             """), {"id": current_user.id}).fetchone()
             
             if result is None or any(valor is None for valor in result):
-                flash("Seu perfil ainda não está completo. Preencha seus dados para continuar.",
+                flash("Seu perfil ainda não está completo. Preencha seus dados para acessar todas as funcionalidades",
                       category="warning")
                 return redirect(url_for('perfil.perfil_page'))
             
