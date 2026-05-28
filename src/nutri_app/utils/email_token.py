ea4ed7d2fe810d2bca09bee4b7705484dbbd1ba4
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-05-28 19:22:06 -0300

    Adiciona UUID único na geração e validação de tokens

--- src/nutri_app/utils/email_token.py
+++ src/nutri_app/utils/email_token.py
@@ -1,14 +1,23 @@
 from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 from flask import current_app
+import uuid
 
 def gerar_token(email):
     s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
-    return s.dumps(email, salt='reset-senha')
+    
+    payload = {
+        "email": email,
+        "uuid": str(uuid.uuid4())
+    }
+
+    return s.dumps(payload, salt='reset-senha')
 
 def validar_token(token, max_age=1800):
     s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    
     try:
-        email = s.loads(token, salt='reset-senha', max_age=max_age)
-        return email
+        data = s.loads(token, salt='reset-senha', max_age=max_age)
+        return data["email"]
+    
     except (BadSignature, SignatureExpired):
         return None
