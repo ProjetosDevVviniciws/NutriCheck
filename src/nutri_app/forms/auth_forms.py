b477696e90517f568acd9af6f2aeb9102ed0a7c0
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-06-10 22:43:27 -0300

    Corrige validação do campo nome; remove validação de senha e adiciona mensagem de confirmação

--- src/nutri_app/forms/auth_forms.py
+++ src/nutri_app/forms/auth_forms.py
@@ -6,28 +6,22 @@ from sqlalchemy import text
 
 class CadastroForm(FlaskForm):
     
-    def validate_usuario(self, check_user):
+    def validate_nome(self, check_user):
         with engine.connect() as coon:
             user = coon.execute(text("SELECT * FROM usuarios WHERE nome = :nome"), {"nome": check_user.data})
             if user.first():
-                raise ValidationError("Usuário já existe! Cadastre outro nome de usuário.")
+                raise ValidationError("Usuário já existe! Cadastre outro nome de usuário")
             
     def validate_email(self, check_email):
         with engine.connect() as coon:
             email = coon.execute(text("SELECT * FROM usuarios WHERE email = :email"), {"email": check_email.data})
             if email.first():
-                raise ValidationError("E-mail já existe! Cadastre outro e-mail.")
-            
-    def validate_senha(self, check_senha):
-        with engine.connect() as coon:
-            senha = coon.execute(text("SELECT * FROM usuarios WHERE senha = :senha"), {"senha": check_senha.data})
-            if senha.first():
-                raise ValidationError("Senha já existe! Cadastre outra senha.")
+                raise ValidationError("E-mail já existe! Cadastre outro e-mail")
     
     nome = StringField(label='Nome', validators=[Length(min=2, max=30), DataRequired()])
     email = StringField(label='Email', validators=[Email(), DataRequired()])
     senha1 = PasswordField(label='Senha', validators=[Length(min=6), DataRequired()])
-    senha2 = PasswordField(label='Confirmação da Senha', validators=[EqualTo('senha1'), DataRequired()])
+    senha2 = PasswordField(label='Confirmação da Senha', validators=[EqualTo('senha1', message='As senhas informadas não coincidem'), DataRequired()])
     submit = SubmitField(label='Cadastrar')
     
 class LoginForm(FlaskForm):
