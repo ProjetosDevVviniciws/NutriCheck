87445cac3f357888462fedc1dcfd86e17eeac875
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-06-14 21:44:14 -0300

    Atualiza importação de utilitários e adiciona validações na atualização de perfil

--- src/nutri_app/routes/perfil_routes.py
+++ src/nutri_app/routes/perfil_routes.py
@@ -2,9 +2,11 @@ from flask import Blueprint, render_template, request, jsonify
 from src.nutri_app.database import engine
 from sqlalchemy import text
 from flask_login import login_required, current_user
-from src.nutri_app.utils.macros import calcular_tmb_macros
+from nutri_app.utils.calcular_tmb import calcular_tmb_macros
+from src.nutri_app.utils.validators import validar_nova_senha
 from src.nutri_app.utils.hash import gerar_hash
 from datetime import date
+import re
 
 perfil_bp = Blueprint('perfil', __name__)
 
@@ -34,7 +36,24 @@ def perfil_atualizar():
     idade = int(data.get("idade"))
     sexo = data.get("sexo")
     nova_senha = data.get("senha")
+    
+    if not nome or not nome.strip():
+        return jsonify({
+            "message": "Esse campo não pode ficar vazio"
+        }), 400
 
+    if not re.match(r"^[A-Za-zÀ-ÿ\s]+$", nome):
+        return jsonify({
+            "message": "O nome deve conter apenas letras"
+        }), 400
+
+    if nova_senha:
+        valida, mensagem = validar_nova_senha(nova_senha)
+        if not valida:
+            return jsonify({
+                "message": mensagem
+            }), 400
+            
     macros = calcular_tmb_macros(peso=peso, altura=altura, idade=idade, sexo=sexo)
     senha_hash = gerar_hash(nova_senha) if nova_senha else None
 
