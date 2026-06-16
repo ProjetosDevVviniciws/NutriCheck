e6573f471c40494d636d404f200b6448d0705da7
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-06-16 14:15:30 -0300

    Remove rotas de recuperação e redefinição de senha após separação de módulos

--- src/nutri_app/routes/login_routes.py
+++ src/nutri_app/routes/login_routes.py
@@ -3,13 +3,8 @@ from src.nutri_app.database import engine
 from src.nutri_app.utils.hash import verificar_senha
 from src.nutri_app.utils.user_login import UserLogin
 from src.nutri_app.forms.auth_forms import LoginForm
-from src.nutri_app.forms.auth_forms import RecuperarSenhaForm, RedefinirSenhaForm
-from nutri_app.utils.email_token import gerar_token, validar_token
-from src.nutri_app.utils.email_service import enviar_email_reset
-from src.nutri_app.utils.hash import gerar_hash
 from sqlalchemy import text
 from flask_login import login_user
-from flask import request
 
 login_bp = Blueprint('login', __name__)
 
@@ -35,100 +30,3 @@ def login():
                 return redirect(url_for('login.login'))
                 
     return render_template("pages/login.html", form=forms)
-
-@login_bp.route("/recuperar-senha", methods=["GET", "POST"])
-def recuperar_senha():
-    form = RecuperarSenhaForm()
-    if form.validate_on_submit():
-        email = form.email.data
-
-        with engine.connect() as conn:
-            user = conn.execute(text("""
-                SELECT * FROM usuarios WHERE email = :email
-            """), {"email": email}).mappings().first()
-
-        if user:
-            token = gerar_token(email)
-            with engine.begin() as conn:
-                conn.execute(text("""
-                    UPDATE usuarios
-                    SET 
-                        reset_token = :token
-                    WHERE email = :email
-                """), {
-                    "token": token,
-                    "email": email
-                })
-                
-            link = url_for("login.redefinir_senha", token=token, _external=True)
-
-            enviar_email_reset(user["email"], link, user["nome"])
-
-        flash("Se o email existir, um link de recuperação será enviado", "info")
-        return redirect(url_for("login.login"))
-    
-    if request.method == "POST" and form.errors:
-        flash(form.email.errors[0], "danger")
-        return redirect(url_for("login.recuperar_senha"))
-
-    return render_template("pages/recuperar_senha.html", form=form)
-
-@login_bp.route("/redefinir-senha/<token>", methods=["GET", "POST"])
-def redefinir_senha(token):
-    form = RedefinirSenhaForm()
-    email = validar_token(token)
-
-    if not email:
-        flash("Token inválido ou expirado", "danger")
-        return redirect(url_for("login.login"))
-
-    with engine.connect() as conn:
-
-        user = conn.execute(text("""
-            SELECT *
-            FROM usuarios
-            WHERE email = :email
-        """), {
-            "email": email
-        }).mappings().first()
-
-    if not user:
-        flash("Usuário não encontrado", "danger")
-        return redirect(url_for("login.login"))
-
-    if not user["reset_token"]:
-        flash(
-            "Este link de recuperação já foi utilizado",
-            "info"
-        )
-        return redirect(url_for("login.login"))
-
-    if user["reset_token"] != token:
-        flash("Link inválido", "danger")
-        return redirect(url_for("login.login"))
-    
-    if form.validate_on_submit():
-        nova_senha = form.senha.data
-        senha_hash = gerar_hash(nova_senha)
-
-        with engine.begin() as conn:
-            conn.execute(text("""
-                UPDATE usuarios
-                SET senha = :senha,
-                reset_token = NULL
-                WHERE email = :email
-            """), {
-                "senha": senha_hash,
-                "email": email
-            })
-
-        flash("Senha atualizada com sucesso!", "info")
-        return redirect(url_for("login.login"))
-
-    if form.errors != {}:
-        for errors in form.errors.values():
-            for err in errors:
-                flash(f"Erro ao atualizar senha: {err}", category="danger")
-            return redirect(url_for("login.redefinir_senha", token=token))
-    
-    return render_template("pages/redefinir_senha.html", form=form)
