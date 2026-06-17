906612dff19ba4d9e6ed5c5639264b82d76e6fd4
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-06-16 22:09:15 -0300

    Atualiza import de módulo após renomeação

--- src/nutri_app/routes/progressao_routes.py
+++ src/nutri_app/routes/progressao_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify
-from src.nutri_app.utils.decorators import perfil_completo_required
+from nutri_app.utils.validacao_perfil import perfil_completo_required
 from flask_login import login_required, current_user
 from sqlalchemy import text
 from src.nutri_app.database import engine
